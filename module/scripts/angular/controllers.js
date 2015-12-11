5a486554f98237d36f777acee73ca5121a8dc9ca
Christian Bors <christianbors@gmail.com>
2015-12-11 16:04:17 +0100

    mess around with angular and OR

---
+++ module/scripts/angular/controllers.js
@@ -0,0 +1,10 @@
+angular.module('metricDocExtension', [])
+	// .directive('myRepeatDirective', function() {
+	// 	return function(scope, element) {
+	// 		markValues(scope, element);
+	// 	}
+	// });
+
+.controller('metricDocExtensionCtrl', function ($scope, $http)) {
+	$http.post()
+}
--- module/scripts/metrics-tool.js
+++ module/scripts/metrics-tool.js
@@ -2,11 +2,118 @@ $(document).ready(function() {
   //$('#demo').html( '<table cellpadding="0" cellspacing="0" border="0" class="display" id="example"></table>' );
 
   var dataSet = [];
-  var columns = [];
+  var columnStore = [];
 
-  var params = {
-    project: 2421247403318
+  URL.getParameters = function () {
+    var r = {};
+
+    var params = window.location.search;
+    if (params.length > 1) {
+        params = params.substr(1).split("&");
+        $.each(params, function () {
+                pair = this.split("=");
+                r[pair[0]] = unescape(pair[1]);
+            });
+    }
+
+    return r;
   };
+
+  var theProject = { id : URL.getParameters().project};
+
+  $.getJSON(
+    "../../command/core/get-project-metadata?" + $.param({ project: theProject.id }), null,
+    function(data) {
+      if (data.status == 'error') {
+        alert(data.message);
+        if (fError) {
+          fError();
+        }
+      } else {
+        theProject.metadata = data;
+        $.getJSON(
+          "../../command/core/get-models?" + $.param({ project: theProject.id }), null,
+          function(data) {
+            for (var n in data) {
+              if (data.hasOwnProperty(n)) {
+                theProject[n] = data[n];
+              }
+            }
+                    
+            var columns = theProject.columnModel.columns;
+            for (var col = 0; col < columns.length; col++) {
+              var column = columns[col];
+              columnStore[column.cellIndex] = {"title": column.name};
+            }
+
+            var dataCols = dataSet[0];
+
+            // if(dataCols) {
+            //   if(dataCols.length == columnStore.length) {
+            //     $('#dataset').dataTable( {
+            //       "data": dataSet,
+            //       "columns": columnStore
+            //     } );
+            //   }
+            // }
+
+            //load column names into the modal
+            for(var col = 0; col < columns.length; col++) {
+              var colName = columns[col];
+              $("<option />")
+             .attr("value", colName.title)
+             .attr("label", colName.title)
+             .appendTo("#columnFormMetricModal");
+            }
+            /*
+            for (var i = 0; i < columns.length; i++) {
+              var column = columns[i];
+              if (column.cellIndex == cellIndex) {
+                return column;
+              }
+            }*/
+
+            $.post(
+              "../../command/core/get-rows?" + $.param({ project: theProject.id, start: 0, limit: 100 }) + "&callback=?",
+              [],
+              function(data) {
+                var rowModel = data;
+
+                // Un-pool objects
+                for (var r = 0; r < data.rows.length; r++) {
+                  var row = data.rows[r];
+                  var rowValues = [];
+                  for (var c = 0; c < theProject.columnModel.columns.length; c++) {
+                    var cell = row.cells[c];
+                    if (cell != null) {
+                      rowValues.push(cell.v);
+                    } else {
+                      rowValues.push('');
+                    }
+                  }
+                  dataSet.push(rowValues);
+                }
+
+                var dataCols = dataSet[0];
+
+                if(columnStore) {
+                  if(dataCols.length == columnStore.length) {
+                    $('#dataset').dataTable( {
+                      "data": dataSet,
+                      "columns": columnStore
+                    } );
+                  }
+                }
+              },
+              "jsonp"
+            );
+          },
+          'json'
+        );
+      }
+    },
+    'json'
+  );
 
   Sortable.create(simpleList, { /* options */ });
   Sortable.create(completenessList, {});
@@ -33,7 +140,8 @@ $(document).ready(function() {
         }, 300);
     });
 
-  $.getJSON("../../command/core/get-columns-info?" + $.param(params),function(data) {
+/*
+  $.getJSON("../../command/core/get-columns-info?" + $.param({ project: theProject.id }),function(data) {
     for (var col = 0; col < data.length; col++) {
       var column = data[col];
       columns[col] = {"title": column.name};
@@ -58,38 +166,7 @@ $(document).ready(function() {
      .attr("label", colName.title)
      .appendTo("#columnFormMetricModal");
     }
-  });
-
-  $.post(
-    "../../command/core/get-rows?" + $.param({ project: 2421247403318, start: 0, limit: 100 }) + "&callback=?",
-    [],
-    function(data) {
-      var rowModel = data;
-
-      // Un-pool objects
-      for (var r = 0; r < data.rows.length; r++) {
-        var row = data.rows[r];
-        var rowValues = [];
-        for (var c = 0; c < row.cells.length; c++) {
-          var cell = row.cells[c];
-          rowValues.push(cell.v);
-        }
-        dataSet.push(rowValues);
-      }
-
-      var dataCols = dataSet[0];
-
-      if(columns) {
-        if(dataCols.length == columns.length) {
-          $('#dataset').dataTable( {
-            "data": dataSet,
-            "columns": columns
-          } );
-        }
-      }
-    },
-    "jsonp"
-  );
+  }); */
 
 } );
 
